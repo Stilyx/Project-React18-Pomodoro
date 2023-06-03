27561e4a37307ca71a1e664de81c6818a7e7c188
Daniel Nunes <dani.perfil.profissional@gmail.com>
2023-06-03 16:51:44 -0300

    add: stopWork, useIterval, useEffect & isWorking

--- src/components/pomodoroTimer/PomodoroTimer.tsx
+++ src/components/pomodoroTimer/PomodoroTimer.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {CircularProgressbar, buildStyles} from 'react-circular-progressbar';
+
+// Hooks
+import UseInterval from '../../hooks/SetInterval';
 
 // Styles
 import './PomodoroTimer.css';
@@ -10,10 +13,62 @@ import {formatedTime} from '../../utils/formatedTime';
 // Types
 import {TPomodoroTimer} from '../../types/TPomodoroTimer';
 
+// Assets
+
+const playSong = require('../../sounds/bell-start.mp3');
+const audioStartWorking = new Audio(playSong);
+const finishSong = require('../../sounds/bell-finish.mp3');
+const audioFinishWork = new Audio(finishSong);
+
 function PomodoroTimer(obj: TPomodoroTimer): JSX.Element {
+	const [cyclesToLongRest, setCyclesToLongRest] = useState<boolean[]>(new Array(4).fill(true));
+	UseInterval(
+		() => {
+			obj.setMainTime(obj.mainTime - 1);
+		},
+		obj.timeCounting ? 1000 : null
+	);
+
+	const stopOrWork = useCallback((): void => {
+		obj.setTimeCounting(true);
+		obj.setWorking(true);
+		obj.setMainTime(obj.defaultPomodoro);
+		obj.setMaxValue(obj.defaultPomodoro);
+		obj.setPomodoroRadio(true);
+		obj.setLongRadio(false);
+		obj.setShortRadio(false);
+		obj.setRest(false);
+	}, [obj]);
+
+	useEffect(() => {
+		if (obj.mainTime > 0) return;
+		audioFinishWork.play();
+
+		if (!obj.isAutomatic) return;
+
+		if (obj.working && cyclesToLongRest.length > 0) {
+			obj.restTime(false);
+			cyclesToLongRest.pop();
+			obj.setCompletedCycles(obj.completedCycles + 1);
+		}
+
+		if (obj.working && cyclesToLongRest.length <= 0) {
+			obj.restTime(true);
+			setCyclesToLongRest(new Array(4).fill(true));
+		}
+
+		if (obj.rest) stopOrWork();
+	}, [obj, cyclesToLongRest, stopOrWork]);
+
+	const isWorking = () => {
+		obj.setTimeCounting(!obj.timeCounting);
+		audioStartWorking.play();
+		if (!obj.working && !obj.rest) stopOrWork();
+	};
+
 	return (
 		<div className='timer-container'>
-			<div onClick={e => obj.isWorking()} className='timer'>
+			<div onClick={e => isWorking()} className='timer'>
 				<CircularProgressbar
 					value={obj.mainTime}
 					text={formatedTime(obj.mainTime)}
