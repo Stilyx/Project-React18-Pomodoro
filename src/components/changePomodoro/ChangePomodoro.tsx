cc70d71ba35a585d54491854e508934cbf3772a4
Daniel Nunes <dani.perfil.profissional@gmail.com>
2023-06-03 16:51:03 -0300

    refatoring

--- src/components/changePomodoro/ChangePomodoro.tsx
+++ src/components/changePomodoro/ChangePomodoro.tsx
@@ -7,9 +7,9 @@ import './ChangePomodoro.css';
 import {TChangePomorodo} from '../../types/TChangePomodoro';
 
 function ChangePomodoro(obj: TChangePomorodo): JSX.Element {
-	const changeRadios = (e: FormEvent<HTMLFormElement>) => {
-		if (e.target instanceof Element) {
-			if (e.target.id === 'pomodoro') {
+	const changeRadios = (event: FormEvent<HTMLInputElement>) => {
+		switch (event.currentTarget.id) {
+			case 'pomodoro':
 				obj.setPomodoroRadio(true);
 				obj.setShortRadio(false);
 				obj.setLongRadio(false);
@@ -17,8 +17,8 @@ function ChangePomodoro(obj: TChangePomorodo): JSX.Element {
 				obj.setMaxValue(obj.defaultPomodoro);
 				obj.setTimeCounting(false);
 				obj.setWorking(true);
-			}
-			if (e.target.id === 'shortBreak') {
+				break;
+			case 'shortBreak':
 				obj.setPomodoroRadio(false);
 				obj.setShortRadio(true);
 				obj.setLongRadio(false);
@@ -27,9 +27,8 @@ function ChangePomodoro(obj: TChangePomorodo): JSX.Element {
 				obj.setTimeCounting(false);
 				obj.setWorking(false);
 				obj.restTime(false);
-			}
-
-			if (e.target.id === 'longBreak') {
+				break;
+			case 'longBreak':
 				obj.setPomodoroRadio(false);
 				obj.setShortRadio(false);
 				obj.setLongRadio(true);
@@ -37,21 +36,38 @@ function ChangePomodoro(obj: TChangePomorodo): JSX.Element {
 				obj.setMaxValue(obj.defaultLongRestTime);
 				obj.setTimeCounting(false);
 				obj.restTime(true);
-			}
+				break;
 		}
 	};
 
 	return (
-		<form
-			action='#'
-			className={obj.isAutomatic ? 'header-radios automaticRadio' : 'header-radios'}
-			onChange={e => (obj.isAutomatic ? '' : changeRadios(e))}
-		>
-			<input type='radio' name='pomodorosTypes' id='pomodoro' checked={obj.pomodoroRadio} />
+		<form action='#' className={obj.isAutomatic ? 'header-radios automaticRadio' : 'header-radios'}>
+			<input
+				type='radio'
+				name='pomodorosTypes'
+				id='pomodoro'
+				checked={obj.pomodoroRadio}
+				onChange={e => changeRadios(e)}
+				disabled={obj.isAutomatic}
+			/>
 			<label htmlFor='pomodoro'>pomodoro</label>
-			<input type='radio' name='pomodorosTypes' id='shortBreak' checked={obj.shortRadio} />
+			<input
+				type='radio'
+				name='pomodorosTypes'
+				id='shortBreak'
+				checked={obj.shortRadio}
+				onChange={e => changeRadios(e)}
+				disabled={obj.isAutomatic}
+			/>
 			<label htmlFor='shortBreak'>short break</label>
-			<input type='radio' name='pomodorosTypes' id='longBreak' checked={obj.longRadio} />
+			<input
+				type='radio'
+				name='pomodorosTypes'
+				id='longBreak'
+				checked={obj.longRadio}
+				onChange={e => changeRadios(e)}
+				disabled={obj.isAutomatic}
+			/>
 			<label htmlFor='longBreak'>long break</label>
 		</form>
 	);
