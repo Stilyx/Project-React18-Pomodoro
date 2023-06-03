5a333b03789cceff6ee0475a28413100f833d85b
Daniel Nunes <dani.perfil.profissional@gmail.com>
2023-06-03 16:52:00 -0300

    add: automaticRadiosChange

--- src/components/switchAutoPomodoro/SwitchAutoPomodoro.tsx
+++ src/components/switchAutoPomodoro/SwitchAutoPomodoro.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useCallback} from 'react';
 
 //Styles
 import './SwitchAutoPomodoro.css';
@@ -18,6 +18,30 @@ function SwitchAutoPomodoro(obj: TSwitchAutoPomodoro): JSX.Element {
 		obj.setTimeCounting(false);
 		obj.setCompletedCycles(0);
 	};
+
+	const automaticRadiosChange = useCallback((): void => {
+		if (obj.pomodoroRadio) {
+			obj.setMainTime(obj.defaultPomodoro);
+			obj.setMaxValue(obj.defaultPomodoro);
+		}
+		if (obj.shortRadio) {
+			obj.setMainTime(obj.defaultRestTime);
+			obj.setMaxValue(obj.defaultRestTime);
+		}
+		if (obj.longRadio) {
+			obj.setMainTime(obj.defaultLongRestTime);
+			obj.setMaxValue(obj.defaultLongRestTime);
+		}
+	}, [obj]);
+
+	useEffect(() => {
+		if (obj.mainTime > 0) return;
+		if (!obj.isAutomatic) {
+			obj.setTimeCounting(false);
+			automaticRadiosChange();
+			return;
+		}
+	});
 
 	return (
 		<div className='switch-container'>
