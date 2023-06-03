93638c8133a693385e420dbc201f643d04e7c7e4
Daniel Nunes <dani.perfil.profissional@gmail.com>
2023-06-03 16:54:20 -0300

    add: ITimerCounting, ISetTimerRadio, IRestTime, IRest

--- src/types/TPomodoroTimer.ts
+++ src/types/TPomodoroTimer.ts
@@ -1,11 +1,21 @@
 import {IAutomatic} from '../interfaces/IAutomatic';
 import {ICycles} from '../interfaces/ICycles';
+import {IDefaultTimer} from '../interfaces/IDefaultTimer';
+import {IRest} from '../interfaces/IRest';
+import {IRestTime} from '../interfaces/IRestTime';
+import {ISetTimer} from '../interfaces/ISetTimer';
+import {ISetTimerRadio} from '../interfaces/ISetTimerRadio';
 import {ITimer} from '../interfaces/ITimer';
 import {ITimerCounting} from '../interfaces/ITimerCounting';
-import {IWorking} from '../interfaces/IWorking';
+import {IWork} from '../interfaces/IWork';
 
 export type TPomodoroTimer = ITimer &
-	IWorking &
 	Pick<IAutomatic, 'isAutomatic'> &
-	Pick<ITimerCounting, 'timeCounting'> &
-	Pick<ICycles, 'completedCycles'>;
+	ITimerCounting &
+	ICycles &
+	ISetTimer &
+	IWork &
+	Pick<IDefaultTimer, 'defaultPomodoro'> &
+	ISetTimerRadio &
+	IRestTime &
+	IRest;
